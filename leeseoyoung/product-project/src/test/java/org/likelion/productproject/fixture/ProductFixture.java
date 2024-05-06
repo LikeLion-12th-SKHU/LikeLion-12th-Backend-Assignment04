1c667066539c6e2e1739742b102c3545213adb90
leeseoyoung05 <lsy202415004@gmail.com>
2024-05-06 10:14:35 +0900

    4주차 Assignment - 이서영

--- leeseoyoung/product-project/src/test/java/org/likelion/productproject/fixture/ProductFixture.java
+++ leeseoyoung/product-project/src/test/java/org/likelion/productproject/fixture/ProductFixture.java
@@ -7,21 +7,21 @@ public class ProductFixture {
     public static final Product PRODUCT_1 = new Product(
             1L,
             8809214408716L,
-            "미니벨",
-            "1500"
+            "초콜릿",
+            "1000"
     );
 
     public static final Product PRODUCT_2 = new Product(
             2L,
             8801062640829L,
-            "크런키",
+            "초콜릿 크런키",
             "2000"
     );
 
     public static final Product PRODUCT_3 = new Product(
             3L,
             8801117352806L,
-            "아이셔",
-            "3500"
+            "밀크 초콜릿",
+            "3000"
     );
 }
