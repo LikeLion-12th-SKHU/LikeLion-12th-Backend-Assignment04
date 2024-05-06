46d5852b5c9cac4eb3d92baf120d737598de09f1
leeseoyoung05 <lsy202415004@gmail.com>
2024-05-06 10:09:41 +0900

    4주차 Assignment - 이서영

---
+++ leeseoyoung/product-project/src/main/java/org/likelion/productproject/ProductProjectService.java
@@ -0,0 +1,13 @@
+package org.likelion.productproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductProjectService {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProductProjectService.class, args);
+    }
+
+}
---
+++ leeseoyoung/product-project/src/test/java/org/likelion/productproject/fixture/ProductFixture.java
@@ -0,0 +1,27 @@
+package org.likelion.productproject.fixture;
+
+import org.likelion.productproject.domain.Product;
+
+public class ProductFixture {
+
+    public static final Product PRODUCT_1 = new Product(
+            1L,
+            8809214408716L,
+            "미니벨",
+            "1500"
+    );
+
+    public static final Product PRODUCT_2 = new Product(
+            2L,
+            8801062640829L,
+            "크런키",
+            "2000"
+    );
+
+    public static final Product PRODUCT_3 = new Product(
+            3L,
+            8801117352806L,
+            "아이셔",
+            "3500"
+    );
+}
