a5d1f48cac5a0c8c3edf604f98878ce274d51dc4
shlee5002 <164116566+shlee5002@users.noreply.github.com>
2024-05-06 16:11:11 +0900

    Update ProductSaveRequestDto.java

--- leeseunghyun/product-project/src/main/java/com/example/productproject/dto/ProductSaveRequestDto.java
+++ leeseunghyun/product-project/src/main/java/com/example/productproject/dto/ProductSaveRequestDto.java
@@ -6,20 +6,18 @@ import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
+
 public class ProductSaveRequestDto {
 
     private Long productSerialNumber;
     private Long productPrice;
     private String productName;
 
-    public ProductSaveRequestDto(Long productSerialNumber, Long productPrice, String productName) {
+@Builder 
+public ProductSaveRequestDto(Long productSerialNumber, Long productPrice, String productName) {
         this.productSerialNumber = productSerialNumber;
         this.productPrice = productPrice;
         this.productName = productName;
     }
 
-
-    public static Object builder() {
-        return 0;
-    }
 }
